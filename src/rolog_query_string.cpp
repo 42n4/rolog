94496e5a9633863cf2817f590889e67d7bf06b95
mgondan <Matthias.Gondan@psy.ku.dk>
2017-02-20 20:52:46 +0100

    Add files via upload

---
+++ src/rolog_query_string.cpp
@@ -0,0 +1,18 @@
+#include <Rcpp.h>
+#include "SWI-Prolog.h"
+#include "SWI-cpp.h"
+
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+List rolog_query_string(String predicate, StringVector arguments) 
+{
+  PlTermv arg(2) ;
+  arg[0] = PlString(String(arguments[0]).get_cstring()) ;
+  PlQuery q(predicate.get_cstring(), arg) ;
+
+  List r ;
+  while(q.next_solution())
+    r.push_back(String((wchar_t*) arg[1])) ;
+  return r ;
+} // rolog_query_string
