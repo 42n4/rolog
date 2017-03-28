f3431f9bdf600671d75a60df774fdfe744f2efdc
mgondan <Matthias.Gondan@psy.ku.dk>
2017-03-28 13:27:41 +0200

    Add files via upload

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -18,6 +18,11 @@ rolog_consult <- function(prolog_files)
   .Call('rolog_rolog_consult', prolog_files, PACKAGE = 'rolog')
 }
 
+rolog_query_LL <- function(predicate, arguments) 
+{
+  .Call('rolog_rolog_query_LL', enc2native(predicate), enc2native(arguments), PACKAGE='rolog')
+}
+
 rolog_query_comp <- function(predicate, arguments) 
 {
   .Call('rolog_rolog_query_comp', enc2native(predicate), enc2native(arguments), PACKAGE='rolog')
