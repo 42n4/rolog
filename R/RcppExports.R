f965d0f0e5568bb4b55630e7933a8df653b66b55
mgondan <Matthias.Gondan@psy.ku.dk>
2017-05-03 18:48:18 +0200

    Update RcppExports.R

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -3,7 +3,7 @@ rcpp_hello_world <- function()
   .Call('rolog_rcpp_hello_world', PACKAGE = 'rolog')
 }
 
-rolog_init <- function(home=system.file("swipl/lib/swipl-7.5.4", package="rolog"), prc=file.path(home, "boot64.prc"))
+rolog_init <- function(home=system.file("swipl/lib/swipl-7.5.5", package="rolog"), prc=file.path(home, "boot64.prc"))
 {
   .Call('rolog_rolog_init', home, prc, PACKAGE = 'rolog')
 }
