73ac9d1400c56f83ae1ae1b437b97e6ed397082f
mgondan <Matthias.Gondan@psy.ku.dk>
2017-07-22 11:34:27 +0200

    Add files via upload

--- src/rolog_query_LL.cpp
+++ src/rolog_query_LL.cpp
@@ -5,19 +5,63 @@
 
 using namespace Rcpp;
 
+SEXP rolog_get(PlTerm t)
+{
+  if(PL_is_integer(t))
+    return(IntegerVector::create(t)) ;
+  
+  if(PL_is_number(t))
+    return(NumericVector::create(t)) ;
+  
+  if(PL_is_string(t))
+  {
+    CharacterVector v(1) ;
+    v[0] = (wchar_t*) t ;
+    return(v) ;
+  }
+  
+  if(PL_is_atom(t))
+  {
+    CharacterVector v(1) ;
+    v[0] = (wchar_t*) t ;
+    v.names() = "Atom" ;
+    return(v) ;
+  }
+  
+  if(PL_is_list(t))
+  {
+    List l ;
+    term_t head = PL_new_term_ref() ;   
+    term_t list = PL_copy_term_ref(t) ; 
+    while(PL_get_list(list, head, list))
+      l.push_back(rolog_get(head)) ;
+
+    return(l) ;
+  }
+  
+  if(PL_is_compound(t))
+  {
+    List l ;
+    l.push_back(t.name()) ;
+    l.names() = "Function" ;
+    for(int i=1 ; i<=t.arity() ; i++)
+      l.push_back(rolog_get(t[i])) ;
+    return(l) ;
+  }
+  
+  return(NumericVector(0)) ;
+}
+  
 // [[Rcpp::export]]
 List rolog_query_LL(String predicate, StringVector arguments) 
 {
-  PlTermv arg(arguments.length()) ;
-  for(int i=0 ; i<arguments.length() ; i++)
-    if(String(arguments[i]) != String(""))
-      arg[i] = PlCompound(String(arguments[i]).get_cstring()) ;
+  PlTermv arg(2) ;
+  arg[0] = PlCompound(String(arguments[0]).get_cstring()) ;
   PlQuery q(predicate.get_cstring(), arg) ;
-
+    
   List r ;
   while(q.next_solution())
-    for(int i=0 ; i<arguments.length() ; i++)
-      if(String(arguments[i]) == String(""))
-        r.push_back(String((wchar_t*) arg[i])) ;
+    r.push_back(rolog_get(arg[1])) ;
+
   return r ;
 } // rolog_query_LL
