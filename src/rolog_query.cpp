16872061ff6e0738e53a0604393a00110ebe2352
mgondan <Matthias.Gondan@psy.ku.dk>
2017-09-13 12:16:54 +0200

    Update rolog_query.cpp

--- src/rolog_query.cpp
+++ src/rolog_query.cpp
@@ -7,6 +7,7 @@ using namespace Rcpp;
 
 PlTerm leaf(SEXP l) ;
 
+// translate R expression to Prolog compound
 PlCompound leaf_lang(SEXP l)
 {
   // Count number of arguments
@@ -25,11 +26,29 @@ PlCompound leaf_lang(SEXP l)
   return PlCompound c(pred.c_str(), v) ;
 }
 
+// translate Prolog compound to R expression
+SEXP leaf_lang(PlCompound t)
+{
+  PairList l ;
+  l.push_back(t.name()) ;
+  for(int i=1 ; i<=t.arity() ; i++)
+    l.push_back(leaf(t[i])) ;
+  return(l) ;
+}
+
+// Prolog NA
 PlTerm leaf_na(SEXP l)
 {
   return PlTerm((long) -1) ;
 }
 
+// R NA
+SEXP leaf_na(PlTerm l)
+{
+  return PlTerm(NumericVector::create(-1)) ;
+}
+
+// real number
 PlTerm leaf_real(SEXP l)
 {
   NumericVector v(1) ;
@@ -37,11 +56,23 @@ PlTerm leaf_real(SEXP l)
   return PlTerm((double) (v[0])) ;
 }
 
+// real number
+SEXP leaf_na(PlTerm l)
+{
+  return PlTerm(NumericVector::create(l)) ;
+}
+
+// integer
 PlTerm leaf_int(IntegerVector l)
 {
   IntegerVector v(1) ;
   v[0] = l[0] ;
   return PlTerm((double) v[0]) ;
+}
+
+SEXP leaf_int(PlTerm l)
+{
+  return PlTerm(IntegerVector::create(l)) ;
 }
 
 PlTerm leaf_symbol(SEXP l)
@@ -50,11 +81,24 @@ PlTerm leaf_symbol(SEXP l)
   return PlAtom(v.c_str()) ;
 }
 
+SEXP leaf_symbol(PLAtom l)
+{
+  Symbol v = as<Symbol>(l) ;
+  return v ;
+}
+
 PlTerm leaf_string(StringVector l)
 {
   StringVector v(1) ;
   v[0] = l[0] ;
   return PlString(String(v[0]).get_cstring()) ;
+}
+
+SEXP leaf_string(PLString l)
+{
+  StringVector v(1) ;
+  v[0] = (wchat_t*) l ;
+  return v ;
 }
 
 PlTerm leaf(SEXP l)
@@ -74,6 +118,26 @@ PlTerm leaf(SEXP l)
   if(TYPEOF(l) == STRSXP)
     return leaf_string(l) ;
   
+  return leaf_na(l) ;
+}
+
+SEXP leaf(PlTerm t)
+{
+  if(PL_is_integer(t))
+    return(leaf_int(t)) ;
+
+  if(PL_is_number(t))
+    return(leaf_real(t)) ;
+  
+  if(PL_is_string(t))
+    return leaf_string(t) ;
+  
+  if(PL_is_atom(t))
+    return leaf_symbol(t) ;
+  
+  if(PL_is_compound(t))
+    return leaf_lang(t) ;
+
   return leaf_na(l) ;
 }
 
