9a3eaafdcba4e3420718189072148ded2aaa46ff
mgondan <Matthias.Gondan@psy.ku.dk>
2017-09-13 10:27:21 +0200

    Update rolog_query.cpp

--- src/rolog_query.cpp
+++ src/rolog_query.cpp
@@ -9,26 +9,34 @@ PlTerm leaf(SEXP l) ;
 
 PlCompound leaf_lang(SEXP l)
 {
+  cout << "entering PLCompound" << endl ;
   int i=0 ;
   for(SEXP cons=CDR(l) ; cons != R_NilValue ; cons = CDR(cons))
     i++ ;
+  cout << i << " arguments" << endl ;
   
   PlTermv v(i) ;
   for(SEXP cons=CDR(l) ; cons != R_NilValue ; cons = CDR(cons))
     v[i++] = leaf(CAR(cons)) ;
+  cout << i << " arguments instantiated" << endl ;
 
   Symbol pred = as<Symbol>(CAR(l)) ;
+  cout << pred.c_str() << "/" << i << " initialized" << endl ;
+  
   PlCompound c(pred.c_str(), v) ;
+  cout << "leaving PLCompound" << endl ;
   return c ;
 }
 
 PlTerm leaf_na(SEXP l)
 {
+  cout << "leaf_na" << endl ;
   return PlTerm((long) -1) ;
 }
 
 PlTerm leaf_real(SEXP l)
 {
+  cout << "leaf_real" << endl ;
   NumericVector v(1) ;
   v[0] = as<double>(l) ;
   return PlTerm(v[0]) ;
@@ -36,6 +44,7 @@ PlTerm leaf_real(SEXP l)
 
 PlTerm leaf_int(IntegerVector l)
 {
+  cout << "leaf_int" << endl ;
   IntegerVector v(1) ;
   v[0] = -l[0] ;
   return PlTerm((double) v[0]) ;
@@ -43,12 +52,14 @@ PlTerm leaf_int(IntegerVector l)
 
 PlTerm leaf_symbol(SEXP l)
 {
+  cout << "leaf_symbol" << endl ;
   Symbol v = as<Symbol>(l) ;
   return PlAtom(v.c_str()) ;
 }
 
 PlTerm leaf_string(StringVector l)
 {
+  cout << "leaf_string" << endl ;
   StringVector v(1) ;
   v[0] = l[0] ;
   return PlString(String(v[0]).get_cstring()) ;
@@ -56,6 +67,7 @@ PlTerm leaf_string(StringVector l)
 
 PlTerm leaf(SEXP l)
 {
+  cout << "leaf dispatcher" << endl ;
   if(TYPEOF(l) == LANGSXP)
     return leaf_lang(l) ;
   
@@ -77,7 +89,9 @@ PlTerm leaf(SEXP l)
 // [[Rcpp::export]]
 List rolog_query(String predicate, SEXP call) 
 {
+  cout << "rolog_query" << endl ;
   PlTerm v = leaf(call) ;
+  cout << "rolog_query done" << endl ;
   return List(1) ;
 
   /*
