bed0cc303ddf01a56e44198558cf76bd70cb17d9
mgondan <Matthias.Gondan@psy.ku.dk>
2017-09-13 12:54:29 +0200

    Update rolog_query.cpp

--- src/rolog_query.cpp
+++ src/rolog_query.cpp
@@ -1,5 +1,4 @@
 #include <Rcpp.h>
-#include <Rcpp/PairList.h>
 #include "SWI-Prolog.h"
 #include "SWI-cpp.h"
 #include <iostream>
@@ -30,7 +29,7 @@ PlCompound leaf_lang(SEXP l)
 // translate Prolog compound to R expression
 SEXP leaf_lang(PlCompound t)
 {
-  PairList l ;
+  Pairlist l ;
   l.push_back(t.name()) ;
   for(int i=1 ; i<=t.arity() ; i++)
     l.push_back(leaf(t[i])) ;
