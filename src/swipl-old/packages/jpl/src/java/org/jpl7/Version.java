63b8b12f175b9475ba3ca221dbfd58a9967468f7
Matthias <matthias-gondan@gmx.de>
2017-12-12 12:18:52 +0100

    new swipl

--- src/swipl-old/packages/jpl/src/java/org/jpl7/Version.java
+++
@@ -1,9 +0,0 @@
-// $Id$
-package org.jpl7;
-
-class Version {
-	public final int major = 7;
-	public final int minor = 4; // jref as blob
-	public final int patch = 0;
-	public final String status = "alpha";
-}
